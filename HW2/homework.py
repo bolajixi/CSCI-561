4dacf8b7bbac36f6a0f359860768b2fe23c7a177
Bolaji Olawale <bolaji.olawale007@gmail.com>
2024-02-13 17:41:13 -0800

    add result

--- HW2/homework.py
+++ HW2/homework.py
@@ -9,9 +9,11 @@ INPUT_FILE = "input.txt"
 OUTPUT_FILE = "output.txt"
 FILE_WRITE_FORMAT = "w"
 
+result = []
+
 
 elapsed_time = time.time() - start_time
-print(f"\nElapsed Time = {'%.2f' % round(elapsed_time, 2)} seconds")
+print(f"\nElapsed Time = {'%.2f' % round(elapsed_time, 2)} seconds \n\n{result}")
 
 with open(OUTPUT_FILE, FILE_WRITE_FORMAT) as output_file:
     output_file.write(result + "\n")
